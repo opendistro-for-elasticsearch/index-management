b79c290880286c4b579d7023dc4b8190681effda
Drew Baugher <dbbaughe@amazon.com>
2019-05-23 15:49:33 -0700

    Register handlers and create components

--- src/main/kotlin/com/amazon/opendistroforelasticsearch/indexstatemanagement/IndexStateManagementPlugin.kt
+++ src/main/kotlin/com/amazon/opendistroforelasticsearch/indexstatemanagement/IndexStateManagementPlugin.kt
@@ -16,18 +16,40 @@
 package com.amazon.opendistroforelasticsearch.indexstatemanagement
 
 import com.amazon.opendistroforelasticsearch.indexstatemanagement.models.ManagedIndex
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.resthandler.RestGetPolicyAction
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.resthandler.RestIndexPolicyAction
 import com.amazon.opendistroforelasticsearch.jobscheduler.spi.JobSchedulerExtension
 import com.amazon.opendistroforelasticsearch.jobscheduler.spi.ScheduledJobParameter
 import com.amazon.opendistroforelasticsearch.jobscheduler.spi.ScheduledJobParser
 import com.amazon.opendistroforelasticsearch.jobscheduler.spi.ScheduledJobRunner
 import org.apache.logging.log4j.LogManager
+import org.elasticsearch.client.Client
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver
+import org.elasticsearch.cluster.node.DiscoveryNodes
+import org.elasticsearch.cluster.service.ClusterService
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry
+import org.elasticsearch.common.settings.ClusterSettings
+import org.elasticsearch.common.settings.IndexScopedSettings
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.settings.SettingsFilter
+import org.elasticsearch.common.xcontent.NamedXContentRegistry
 import org.elasticsearch.common.xcontent.XContentParser
 import org.elasticsearch.common.xcontent.XContentParserUtils
+import org.elasticsearch.env.Environment
+import org.elasticsearch.env.NodeEnvironment
+import org.elasticsearch.plugins.ActionPlugin
 import org.elasticsearch.plugins.Plugin
+import org.elasticsearch.rest.RestController
+import org.elasticsearch.rest.RestHandler
+import org.elasticsearch.script.ScriptService
+import org.elasticsearch.threadpool.ThreadPool
+import org.elasticsearch.watcher.ResourceWatcherService
+import java.util.function.Supplier
 
-internal class IndexStateManagementPlugin : JobSchedulerExtension, Plugin() {
+internal class IndexStateManagementPlugin : JobSchedulerExtension, ActionPlugin, Plugin() {
 
     private val logger = LogManager.getLogger(javaClass)
+    lateinit var indexStateManagementIndices: IndexStateManagementIndices
 
     companion object {
         const val PLUGIN_NAME = "opendistro-ism"
@@ -65,5 +87,36 @@ internal class IndexStateManagementPlugin : JobSchedulerExtension, Plugin() {
             }
             job
         }
+    }
+
+    override fun getRestHandlers(
+        settings: Settings,
+        restController: RestController,
+        clusterSettings: ClusterSettings,
+        indexScopedSettings: IndexScopedSettings,
+        settingsFilter: SettingsFilter,
+        indexNameExpressionResolver: IndexNameExpressionResolver,
+        nodesInCluster: Supplier<DiscoveryNodes>
+    ): List<RestHandler> {
+        return listOf(
+            RestIndexPolicyAction(settings, restController, indexStateManagementIndices),
+            RestGetPolicyAction(settings, restController)
+        )
+    }
+
+    override fun createComponents(
+        client: Client,
+        clusterService: ClusterService,
+        threadPool: ThreadPool,
+        resourceWatcherService: ResourceWatcherService,
+        scriptService: ScriptService,
+        xContentRegistry: NamedXContentRegistry,
+        environment: Environment,
+        nodeEnvironment: NodeEnvironment,
+        namedWriteableRegistry: NamedWriteableRegistry
+    ): Collection<Any> {
+        val managedIndexRunner = ManagedIndexRunner.instance
+        indexStateManagementIndices = IndexStateManagementIndices(client.admin().indices(), clusterService)
+        return listOf(managedIndexRunner, indexStateManagementIndices)
     }
 }

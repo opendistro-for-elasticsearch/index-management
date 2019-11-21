1cbc71408974c53a328d923949c13ff6affe8e21
Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
2019-11-21 11:18:46 -0800

    Remove unused import (#112)

--- src/main/kotlin/com/amazon/opendistroforelasticsearch/indexstatemanagement/transport/action/updateindexmetadata/TransportUpdateManagedIndexMetaDataAction.kt
+++ src/main/kotlin/com/amazon/opendistroforelasticsearch/indexstatemanagement/transport/action/updateindexmetadata/TransportUpdateManagedIndexMetaDataAction.kt
@@ -38,7 +38,6 @@ import org.elasticsearch.cluster.metadata.MetaData
 import org.elasticsearch.cluster.service.ClusterService
 import org.elasticsearch.common.inject.Inject
 import org.elasticsearch.common.io.stream.StreamInput
-import org.elasticsearch.common.io.stream.Writeable
 import org.elasticsearch.threadpool.ThreadPool
 import org.elasticsearch.transport.TransportService
 import java.util.function.Supplier

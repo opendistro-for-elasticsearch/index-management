3b7c104a18a465ec0e691f55be844f39afc17d13
Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
2020-04-07 14:57:50 -0700

    Adds isIdempotent method to each step and updates ManagedIndexRunner to use it (#165) (#195)

--- src/main/kotlin/com/amazon/opendistroforelasticsearch/indexstatemanagement/step/replicacount/AttemptSetReplicaCountStep.kt
+++ src/main/kotlin/com/amazon/opendistroforelasticsearch/indexstatemanagement/step/replicacount/AttemptSetReplicaCountStep.kt
@@ -38,6 +38,8 @@ class AttemptSetReplicaCountStep(
     private var stepStatus = StepStatus.STARTING
     private var info: Map<String, Any>? = null
 
+    override fun isIdempotent() = true
+
     @Suppress("TooGenericExceptionCaught")
     override suspend fun execute() {
         val numOfReplicas = config.numOfReplicas

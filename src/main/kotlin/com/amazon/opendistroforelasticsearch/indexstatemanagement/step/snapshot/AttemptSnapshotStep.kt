4a4d707ee9cc6a7901a820bff89f94ea69da3e62
Daniel Bryła <31926560+dabr-grapeup@users.noreply.github.com>
2020-05-14 21:05:15 +0200

    Snapshot implementation (#135)
    * Snapshot implementation
    Action has two steps:
    attempt_snapshot and wait_for_snapshot
    
    * Add timestamp to snapshot
    
    * added missing license headers
    replaced LinkedHashMap in SnapshotAction
    handle waitForSnapshotStep is not completed
    added usermetadata
    added hash at the end of the name
    set explicit waitForCompletion
    using CONDITION_NOT_MET
    
    * added snapshot name hash
    
    * implemented isIdempotent method
    
    * Do not fail snapshot if another one is in progress
    + Restore snapshot name with timestamp
    
    * replace wildcard imports and set isIdempotent to false
    
    * removed includeGlobalState from Snapshotaction
    
    Co-authored-by: Daniel Bryla <daniel.bryla@hertz.com>
    Co-authored-by: Javier Abrego <javier.abrego@externals.adidas.com>

---
+++ src/main/kotlin/com/amazon/opendistroforelasticsearch/indexstatemanagement/step/snapshot/AttemptSnapshotStep.kt
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazon.opendistroforelasticsearch.indexstatemanagement.step.snapshot
+
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.elasticapi.suspendUntil
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.model.ManagedIndexMetaData
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.model.action.SnapshotActionConfig
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.model.managedindexmetadata.StepMetaData
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.step.Step
+import org.apache.logging.log4j.LogManager
+import org.elasticsearch.action.admin.cluster.snapshots.create.CreateSnapshotRequest
+import org.elasticsearch.action.admin.cluster.snapshots.create.CreateSnapshotResponse
+import org.elasticsearch.client.Client
+import org.elasticsearch.cluster.service.ClusterService
+import org.elasticsearch.rest.RestStatus
+import org.elasticsearch.snapshots.ConcurrentSnapshotExecutionException
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+class AttemptSnapshotStep(
+    val clusterService: ClusterService,
+    val client: Client,
+    val config: SnapshotActionConfig,
+    managedIndexMetaData: ManagedIndexMetaData
+) : Step(name, managedIndexMetaData) {
+
+    private val logger = LogManager.getLogger(javaClass)
+    private var stepStatus = StepStatus.STARTING
+    private var info: Map<String, Any>? = null
+
+    override fun isIdempotent() = false
+
+    @Suppress("TooGenericExceptionCaught")
+    override suspend fun execute() {
+        try {
+            logger.info("Executing snapshot on ${managedIndexMetaData.index}")
+            val snapshotName = config
+                    .snapshot
+                    .plus("-")
+                    .plus(LocalDateTime
+                            .now(ZoneId.of("UTC"))
+                            .format(DateTimeFormatter.ofPattern("uuuu.MM.dd-HH:mm:ss.SSS", Locale.ROOT)))
+            val mutableInfo = mutableMapOf("snapshotName" to snapshotName)
+
+            val createSnapshotRequest = CreateSnapshotRequest()
+                    .userMetadata(mapOf("snapshot_created" to "Open Distro for Elasticsearch Index Management"))
+                    .indices(managedIndexMetaData.index)
+                    .snapshot(snapshotName)
+                    .repository(config.repository)
+                    .waitForCompletion(false)
+
+            val response: CreateSnapshotResponse = client.admin().cluster().suspendUntil { createSnapshot(createSnapshotRequest, it) }
+            when (response.status()) {
+                RestStatus.ACCEPTED -> {
+                    stepStatus = StepStatus.COMPLETED
+                    mutableInfo["message"] = "Snapshot creation started and is still in progress for index: ${managedIndexMetaData.index}"
+                }
+                RestStatus.OK -> {
+                    stepStatus = StepStatus.COMPLETED
+                    mutableInfo["message"] = "Snapshot created for index: ${managedIndexMetaData.index}"
+                }
+                else -> {
+                    stepStatus = StepStatus.FAILED
+                    mutableInfo["message"] = "There was an error during snapshot creation for index: ${managedIndexMetaData.index}"
+                    mutableInfo["cause"] = response.toString()
+                }
+            }
+            info = mutableInfo.toMap()
+        } catch (e: ConcurrentSnapshotExecutionException) {
+            val message = "Snapshot creation already in progress."
+            logger.debug(message, e)
+            stepStatus = StepStatus.CONDITION_NOT_MET
+            info = mapOf("message" to message)
+        } catch (e: Exception) {
+            val message = "Failed to create snapshot for index: ${managedIndexMetaData.index}"
+            logger.error(message, e)
+            stepStatus = StepStatus.FAILED
+            val mutableInfo = mutableMapOf("message" to message)
+            val errorMessage = e.message
+            if (errorMessage != null) mutableInfo["cause"] = errorMessage
+            info = mutableInfo.toMap()
+        }
+    }
+
+    override fun getUpdatedManagedIndexMetaData(currentMetaData: ManagedIndexMetaData): ManagedIndexMetaData {
+        return currentMetaData.copy(
+                stepMetaData = StepMetaData(name, getStepStartTime().toEpochMilli(), stepStatus),
+                transitionTo = null,
+                info = info
+        )
+    }
+
+    companion object {
+        const val name = "attempt_snapshot"
+    }
+}
---
+++ src/main/kotlin/com/amazon/opendistroforelasticsearch/indexstatemanagement/step/snapshot/WaitForSnapshotStep.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazon.opendistroforelasticsearch.indexstatemanagement.step.snapshot
+
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.elasticapi.suspendUntil
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.model.ManagedIndexMetaData
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.model.action.SnapshotActionConfig
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.model.managedindexmetadata.StepMetaData
+import com.amazon.opendistroforelasticsearch.indexstatemanagement.step.Step
+import org.apache.logging.log4j.LogManager
+import org.elasticsearch.action.admin.cluster.snapshots.status.SnapshotStatus
+import org.elasticsearch.action.admin.cluster.snapshots.status.SnapshotsStatusRequest
+import org.elasticsearch.action.admin.cluster.snapshots.status.SnapshotsStatusResponse
+import org.elasticsearch.client.Client
+import org.elasticsearch.cluster.service.ClusterService
+
+class WaitForSnapshotStep(
+    val clusterService: ClusterService,
+    val client: Client,
+    val config: SnapshotActionConfig,
+    managedIndexMetaData: ManagedIndexMetaData
+) : Step(name, managedIndexMetaData) {
+    private val logger = LogManager.getLogger(javaClass)
+    private var stepStatus = StepStatus.STARTING
+    private var info: Map<String, Any>? = null
+
+    override fun isIdempotent() = true
+
+    override suspend fun execute() {
+        logger.info("Waiting for snapshot to complete...")
+        val request = SnapshotsStatusRequest()
+            .snapshots(arrayOf(managedIndexMetaData.info?.get("snapshotName").toString()))
+            .repository(config.repository)
+        val response: SnapshotsStatusResponse = client.admin().cluster().suspendUntil { snapshotsStatus(request, it) }
+        val status: SnapshotStatus? = response
+            .snapshots
+            .find { snapshotStatus ->
+                snapshotStatus.snapshot.snapshotId.name == managedIndexMetaData.info?.get("snapshotName").toString() &&
+                        snapshotStatus.snapshot.repository == config.repository
+            }
+        if (status != null) {
+            if (status.state.completed()) {
+                stepStatus = StepStatus.COMPLETED
+                info = mapOf("message" to "Snapshot created for index: ${managedIndexMetaData.index}")
+            } else {
+                stepStatus = StepStatus.CONDITION_NOT_MET
+                info = mapOf("message" to "Creating snapshot in progress for index: ${managedIndexMetaData.index}")
+            }
+        } else {
+            stepStatus = StepStatus.FAILED
+            info = mapOf("message" to "Snapshot doesn't exist for index: ${managedIndexMetaData.index}")
+        }
+    }
+
+    override fun getUpdatedManagedIndexMetaData(currentMetaData: ManagedIndexMetaData): ManagedIndexMetaData {
+        return currentMetaData.copy(
+            stepMetaData = StepMetaData(name, getStepStartTime().toEpochMilli(), stepStatus),
+            transitionTo = null,
+            info = info
+        )
+    }
+
+    companion object {
+        const val name = "wait_for_snapshot"
+    }
+}

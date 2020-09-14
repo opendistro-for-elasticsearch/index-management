5ba4763c1d14dc9f286fbaff6f1ef5a465c43f43
bowenlan-amzn <bowenlan@amazon.com>
2020-09-14 15:39:19 -0700

    correct action name

--- src/main/kotlin/com/amazon/opendistroforelasticsearch/indexmanagement/indexstatemanagement/transport/action/removepolicy/RemovePolicyAction.kt
+++ src/main/kotlin/com/amazon/opendistroforelasticsearch/indexmanagement/indexstatemanagement/transport/action/removepolicy/RemovePolicyAction.kt
@@ -5,6 +5,6 @@ import org.elasticsearch.action.ActionType
 class RemovePolicyAction private constructor() : ActionType<RemovePolicyResponse>(NAME, ::RemovePolicyResponse) {
     companion object {
         val INSTANCE = RemovePolicyAction()
-        val NAME = "cluster:admin/ism/policies/delete"
+        val NAME = "cluster:admin/ism/managedindex/remove"
     }
 }

b2fcb4b7b1e318e3edd9aa9f0188c5cc4794f459
bowenlan-amzn <bowenlan@amazon.com>
2020-12-09 07:25:58 -0800

    save progress

--- src/test/kotlin/com/amazon/opendistroforelasticsearch/indexmanagement/indexstatemanagement/resthandler/ISMTemplateRestAPIIT.kt
+++ src/test/kotlin/com/amazon/opendistroforelasticsearch/indexmanagement/indexstatemanagement/resthandler/ISMTemplateRestAPIIT.kt
@@ -1,15 +1,16 @@
 package com.amazon.opendistroforelasticsearch.indexmanagement.indexstatemanagement.resthandler
 
 import com.amazon.opendistroforelasticsearch.indexmanagement.indexstatemanagement.IndexStateManagementRestTestCase
-import org.elasticsearch.common.io.Streams
-import org.elasticsearch.common.xcontent.LoggingDeprecationHandler
-import org.elasticsearch.common.xcontent.NamedXContentRegistry
-import org.elasticsearch.common.xcontent.json.JsonXContent
-import java.io.InputStreamReader
-import java.nio.charset.StandardCharsets
+import com.amazon.opendistroforelasticsearch.indexmanagement.indexstatemanagement.model.ISMTemplate
+import com.amazon.opendistroforelasticsearch.indexmanagement.randomInstant
+import org.elasticsearch.rest.RestStatus
+import java.util.*
 
 class ISMTemplateRestAPIIT : IndexStateManagementRestTestCase() {
-    fun `test create ISM template`() {
+
+    fun `test ISM template`() {
+        val templateName = "t1"
+        println("template name $templateName")
         val ismTemplate = """
             {
                 "index_patterns": ["log*"],
@@ -17,7 +18,9 @@ class ISMTemplateRestAPIIT : IndexStateManagementRestTestCase() {
                 "priority": 100
             }
         """.trimIndent()
-        val res = createISMTemplateJson("t1", ismTemplate)
+        var res = createISMTemplateJson(templateName, ismTemplate)
+        assertEquals("Unable to create new ISM template", RestStatus.CREATED, res.restStatus())
+
         // val str1 = res.entity.content.bufferedReader().readText()
         // val str2 = Streams.copyToString(InputStreamReader(res.entity.content))
         // val map1 = JsonXContent.jsonXContent
@@ -30,15 +33,34 @@ class ISMTemplateRestAPIIT : IndexStateManagementRestTestCase() {
         // println("create template response string2 $str2")
         // println("create template response map1 $map1")
 
-        val ismTemplatesRes = getISMTemplate("t1")
+        res = createISMTemplateJson(templateName, ismTemplate)
+        assertEquals("Unable to update new ISM template", RestStatus.OK, res.restStatus())
+
+
+        val ismTemplatesRes = getISMTemplates(templateName)
         println("get template parsed response $ismTemplatesRes")
-    }
 
-    fun `test get ISM template`() {
+        // createISMTemplateJson("t2", """
+        //     {
+        //         "index_patterns": ["log*"],
+        //         "policy_id": "policy_1",
+        //         "priority": 50
+        //     }
+        // """.trimIndent())
 
-    }
-
-    fun `test delete ISM template`() {
-
+        val mapp1 = getISMTemplatesMap(templateName)
+        println("get template response map $mapp1")
+        assertPredicatesOnISMTemplates(
+            listOf(
+                templateName to listOf(
+                    "template_name" to templateName::equals,
+                    "ism_template" to fun(template: Any?): Boolean = assertISMTemplateEquals(
+                        ISMTemplate(listOf("log*"), "policy_1", 100, randomInstant()),
+                        template
+                    )
+                )
+            ),
+            mapp1
+        )
     }
 }

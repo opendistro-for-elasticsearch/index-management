31889dac9dc58dc43b6bcc115730feb4566b62b5
bowenlan-amzn <bowenlan@amazon.com>
2020-12-09 16:38:08 -0800

    simple tests for request response

---
+++ src/test/kotlin/com/amazon/opendistroforelasticsearch/indexmanagement/indexstatemanagement/transport/action/ismtemplate/get/GetISMTemplateRequestTests.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazon.opendistroforelasticsearch.indexmanagement.indexstatemanagement.transport.action.ismtemplate.get
+
+import org.elasticsearch.common.io.stream.BytesStreamOutput
+import org.elasticsearch.common.io.stream.StreamInput
+import org.elasticsearch.test.ESTestCase
+
+class GetISMTemplateRequestTests : ESTestCase() {
+
+    fun `test get templates request`() {
+        val templateNames = arrayOf("t1")
+        val req = GetISMTemplateRequest(templateNames)
+
+        val out = BytesStreamOutput()
+        req.writeTo(out)
+        val sin = StreamInput.wrap(out.bytes().toBytesRef().bytes)
+        val newReq = GetISMTemplateRequest(sin)
+        assertEquals(templateNames, newReq.templateNames)
+    }
+}

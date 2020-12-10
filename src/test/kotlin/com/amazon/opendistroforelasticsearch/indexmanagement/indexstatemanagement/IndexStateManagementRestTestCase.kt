ff4053547ad5e85ac4b862f1b708cded8ef51c9c
bowenlan-amzn <bowenlan@amazon.com>
2020-12-10 09:48:42 -0800

    wanna see code cov

--- src/test/kotlin/com/amazon/opendistroforelasticsearch/indexmanagement/indexstatemanagement/IndexStateManagementRestTestCase.kt
+++ src/test/kotlin/com/amazon/opendistroforelasticsearch/indexmanagement/indexstatemanagement/IndexStateManagementRestTestCase.kt
@@ -715,12 +715,11 @@ abstract class IndexStateManagementRestTestCase : IndexManagementRestTestCase() 
         name: String,
         templateString: String
     ): Response {
-        val response = client().makeRequest(
+        return client().makeRequest(
             "PUT",
             "$ISM_TEMPLATE_BASE_URI/$name",
             StringEntity(templateString, APPLICATION_JSON)
         )
-        return response
     }
 
     protected fun getISMTemplatesAsMap(name: String): Map<String, Any> {
@@ -810,5 +809,9 @@ abstract class IndexStateManagementRestTestCase : IndexManagementRestTestCase() 
             assertEquals(expected.priority, actual.priority)
         }
         return true
+    }
+
+    protected fun deleteISMTemplate(name: String): Response {
+        return client().makeRequest("DELETE", "$ISM_TEMPLATE_BASE_URI/$name")
     }
 }

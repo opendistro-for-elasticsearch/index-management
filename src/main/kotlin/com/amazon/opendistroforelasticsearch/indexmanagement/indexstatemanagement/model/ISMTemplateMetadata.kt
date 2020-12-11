27b1f6f2aeb0048096d3603cc7e57a7d2a897e4b
bowenlan-amzn <bowenlan@amazon.com>
2020-12-10 17:05:35 -0800

    try remove seeming not used part in template metadata

--- src/main/kotlin/com/amazon/opendistroforelasticsearch/indexmanagement/indexstatemanagement/model/ISMTemplateMetadata.kt
+++ src/main/kotlin/com/amazon/opendistroforelasticsearch/indexmanagement/indexstatemanagement/model/ISMTemplateMetadata.kt
@@ -24,13 +24,8 @@ import org.elasticsearch.cluster.metadata.Metadata
 import org.elasticsearch.common.ParseField
 import org.elasticsearch.common.io.stream.StreamInput
 import org.elasticsearch.common.io.stream.StreamOutput
-import org.elasticsearch.common.xcontent.ConstructingObjectParser
-import org.elasticsearch.common.xcontent.ConstructingObjectParser.constructorArg
-import org.elasticsearch.common.xcontent.ContextParser
 import org.elasticsearch.common.xcontent.ToXContent
 import org.elasticsearch.common.xcontent.XContentBuilder
-import org.elasticsearch.common.xcontent.XContentParser
-import org.elasticsearch.common.xcontent.XContentParserUtils.ensureExpectedToken
 import java.io.IOException
 import java.util.EnumSet
 
@@ -58,7 +53,6 @@ class ISMTemplateMetadata(val ismTemplates: Map<String, ISMTemplate>) : Metadata
     }
 
     override fun toXContent(builder: XContentBuilder, params: ToXContent.Params): XContentBuilder {
-        log.info("ism template metadata toXContent: $ismTemplates")
         builder.startObject(ISM_TEMPLATE.preferredName)
         ismTemplates.forEach { (k, v) ->
             builder.field(k, v)
@@ -84,7 +78,7 @@ class ISMTemplateMetadata(val ismTemplates: Map<String, ISMTemplate>) : Metadata
         @Throws(IOException::class)
         constructor(sin: StreamInput) {
             this.ismTemplateDiff = DiffableUtils.readJdkMapDiff(sin, DiffableUtils.getStringKeySerializer(),
-            ::ISMTemplate, ISMTemplate.Companion::readISMTemplateDiffFrom)
+                    ::ISMTemplate, ISMTemplate.Companion::readISMTemplateDiffFrom)
         }
 
         @Throws(IOException::class)
@@ -102,35 +96,6 @@ class ISMTemplateMetadata(val ismTemplates: Map<String, ISMTemplate>) : Metadata
     companion object {
         val TYPE = "ism_template"
         val ISM_TEMPLATE = ParseField("ism_template")
-
-        val PARSER = ConstructingObjectParser<ISMTemplateMetadata, Void>(TYPE, false
-        ) { a -> ISMTemplateMetadata(a[0] as Map<String, ISMTemplate>) }
-
-        init {
-            PARSER.declareObject(constructorArg(), ContextParser<Void, Map<String, ISMTemplate>> { p, _ ->
-                val templates = mutableMapOf<String, ISMTemplate>()
-                while (p.nextToken() != XContentParser.Token.END_OBJECT) {
-                    val name = p.currentName()
-                    templates[name] = ISMTemplate.parse(p)
-                }
-                templates
-            }, ISM_TEMPLATE)
-        }
-
-        fun fromStreamInput(sin: StreamInput) = ISMTemplateMetadata(sin.readMap(StreamInput::readString, ::ISMTemplate))
-
-        // fun parse(xcp: XContentParser): ISMTemplateMetadata = PARSER.parse(xcp, null)
-        fun parse(xcp: XContentParser): ISMTemplateMetadata {
-            val ismTemplates = mutableMapOf<String, ISMTemplate>()
-            log.info("ism template metadata parse, first token ${xcp.currentToken()}")
-            ensureExpectedToken(XContentParser.Token.START_OBJECT, xcp.nextToken(), xcp)
-            while (xcp.nextToken() != XContentParser.Token.END_OBJECT) {
-                val fieldName = xcp.currentName()
-                log.info("current field name $fieldName")
-                ismTemplates[fieldName] = ISMTemplate.parse(xcp)
-            }
-            return ISMTemplateMetadata(ismTemplates)
-        }
 
         fun readDiffFrom(sin: StreamInput): NamedDiff<Metadata.Custom> = ISMTemplateMetadataDiff(sin)
     }
